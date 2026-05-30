1b3ff8bb3ad892383ab12dc326eb6ca88ea4721f
RafaelToledo09 <rafaeltoledo09@gmail.com>
2026-05-30 15:05:35 -0300

    projeto em execução

--- paginas/identidadevisual.php
+++ paginas/identidadevisual.php
@@ -2,6 +2,8 @@
 
     <main class="espaco-cabecalho container">
 
+        <div class="pbloco">
+
             <img src="/ProjetoLuz/imgs/tituloidentidade.png" data-aos="fade-right" data-aos-duration="2000">
 
             <h2>
@@ -11,8 +13,17 @@ posicionamento visual e a identidade que os diferencia.
 
             <img src="/ProjetoLuz/imgs/banner1.png" alt="Identidade Visual Banner" data-aos="fade-right" data-aos-duration="1700">
             <img src="/ProjetoLuz/imgs/banner2.png" alt="Identidade Visual Banner 2" data-aos="fade-left" data-aos-duration="1900">
-    
+        </div>
 
+        <div class="sbloco">
+            <p>Mais do que um logotipo, a identidade visual constrói reconhecimento, diferenciação e presença estética. É a
+transformação de uma ideia em linguagem visual — onde conceito se torna produto, produto se torna experiência e a
+estética se torna identidade.</p>
+            <img src="/ProjetoLuz/imgs/banneridentidade3.png" alt="Banner3" data-aos="fade-right" data-aos-duration="1700">
+        
+        
+        
+        </div
 
 
 
